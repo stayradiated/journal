f19ca6890050e8c83fd576bb3b03aae8d4757687
George Czabania <1140167+stayradiated@users.noreply.github.com>
2020-11-15 12:10:58 +1300

    feat: add build.sh v0.1

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+plaintext_version='0.1'
+plaintext_url="https://github.com/stayradiated/plaintext/releases/download/v${plaintext_version}/plaintext_${plaintext_version}_linux_386.tar.gz"
+
+if test ! -f /tmp/plaintext; then
+  curl --location --output /tmp/plaintext.tgz "${plaintext_url}"
+  tar --extract --file /tmp/plaintext.tgz --directory=/tmp plaintext
+fi
+
+/tmp/plaintext
+
