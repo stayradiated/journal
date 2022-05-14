602f5c9686432d4ec2a7e869b8b6db94cedc625d
George Czabania <1140167+stayradiated@users.noreply.github.com>
2022-05-14 19:04:55 +0200

    build: use plaintext v0.5.3

--- .build/build.sh
+++ .build/build.sh
@@ -3,7 +3,7 @@
 build_dir=$(cd $(dirname "${BASH_SOURCE[0]}") >/dev/null 2>&1 && pwd)
 template_path="${build_dir}/template.html"
 
-plaintext_version='0.4'
+plaintext_version='0.5.3'
 plaintext_url="https://github.com/stayradiated/plaintext/releases/download/v${plaintext_version}/plaintext_${plaintext_version}_linux_386.tar.gz"
 
 if test ! -f /tmp/plaintext; then
@@ -11,4 +11,4 @@ if test ! -f /tmp/plaintext; then
   tar --extract --file /tmp/plaintext.tgz --directory=/tmp plaintext
 fi
 
-/tmp/plaintext "${template_path}"
+/tmp/plaintext html --template "${template_path}"
