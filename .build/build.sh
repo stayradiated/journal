a43d821a327425ab1f7ecd657c1f334f7962661f
George Czabania <1140167+stayradiated@users.noreply.github.com>
2022-05-14 19:34:50 +0200

    build: use v0.5

--- .build/build.sh
+++ .build/build.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 build_dir=$(cd $(dirname "${BASH_SOURCE[0]}") >/dev/null 2>&1 && pwd)
 template_path="${build_dir}/template.html"
 
-plaintext_version='0.5.3'
+plaintext_version='0.5'
 plaintext_url="https://github.com/stayradiated/plaintext/releases/download/v${plaintext_version}/plaintext_${plaintext_version}_linux_386.tar.gz"
 
 if test ! -f /tmp/plaintext; then
