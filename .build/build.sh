2ba54b710157fec4d47a197038d9b758efef3b2d
George Czabania <1140167+stayradiated@users.noreply.github.com>
2022-05-14 19:18:00 +0200

    build: try using tar --ungzip

--- .build/build.sh
+++ .build/build.sh
@@ -7,8 +7,8 @@ plaintext_version='0.5.3'
 plaintext_url="https://github.com/stayradiated/plaintext/releases/download/v${plaintext_version}/plaintext_${plaintext_version}_linux_386.tar.gz"
 
 if test ! -f /tmp/plaintext; then
-  curl --location --output /tmp/plaintext.tgz "${plaintext_url}"
-  tar --extract --file /tmp/plaintext.tgz --directory=/tmp plaintext
+  curl --location --output /tmp/plaintext.tar.gz "${plaintext_url}"
+  tar --ungzip --extract --file /tmp/plaintext.tar.gz --directory=/tmp plaintext
 fi
 
 /tmp/plaintext html --template "${template_path}"
